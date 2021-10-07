845d071c61927441a8ef95e595a68ef8ed030cda
UniversalDon <56935053+UniversalDon@users.noreply.github.com>
2021-10-08 00:07:15 +0530

    Create tubelight.cpp

---
+++ c++/tubelight.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout<<"Hi vro, please turn on the lights\n";
+  
+  cout<<"I am having hard time seeing the characters of my book";
+	
+	return 0;
+}
